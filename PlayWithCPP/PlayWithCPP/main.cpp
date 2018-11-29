04838b77d7b904a64073e0c60db691c5a5e24095
Mike Laursen <mailbox@laursen.org>
2018-11-29 15:21:16 -0800

    Generated project

---
+++ PlayWithCPP/PlayWithCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlayWithCPP
+//
+//  Created by Mike Laursen on 11/29/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
