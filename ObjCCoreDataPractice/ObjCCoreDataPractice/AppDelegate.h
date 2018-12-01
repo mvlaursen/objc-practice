558efd0cfb5adfed82d8b60492000fc21ea2ebe9
Mike Laursen <mailbox@laursen.org>
2018-12-01 13:37:32 -0800

    Generate project

---
+++ ObjCCoreDataPractice/ObjCCoreDataPractice/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ObjCCoreDataPractice
+//
+//  Created by Mike Laursen on 12/1/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
