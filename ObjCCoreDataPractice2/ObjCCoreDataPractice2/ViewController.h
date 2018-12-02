d12c2909cec7b21d52e423d28fdf2e1471c04906
Mike Laursen <mailbox@laursen.org>
2018-12-02 15:54:37 -0800

    Generated project

---
+++ ObjCCoreDataPractice2/ObjCCoreDataPractice2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCCoreDataPractice2
+//
+//  Created by Mike Laursen on 12/2/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
