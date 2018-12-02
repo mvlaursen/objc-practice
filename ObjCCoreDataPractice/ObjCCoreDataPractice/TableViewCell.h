406a98a0592c9b97575b834a7f91ad7fa5e65b63
Mike Laursen <mailbox@laursen.org>
2018-12-02 14:56:39 -0800

    Set up a table view to display the entities in Core Data.

---
+++ ObjCCoreDataPractice/ObjCCoreDataPractice/TableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.h
+//  ObjCCoreDataPractice
+//
+//  Created by Mike Laursen on 12/2/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ObjCCoreDataPractice/ObjCCoreDataPractice/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  ObjCCoreDataPractice
+//
+//  Created by Mike Laursen on 12/2/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
