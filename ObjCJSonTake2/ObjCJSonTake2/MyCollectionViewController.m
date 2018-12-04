f1c085450f616408456e9cbdfcb0accb6b3ca80e
Mike Laursen <mailbox@laursen.org>
2018-12-04 14:34:06 -0800

    WTF. You have to comment out the "registerClass" line in viewDidLoad, or it sabotages the cell layout.

--- ObjCJSonTake2/ObjCJSonTake2/MyCollectionViewController.m
+++ ObjCJSonTake2/ObjCJSonTake2/MyCollectionViewController.m
@@ -14,7 +14,7 @@
 
 @implementation MyCollectionViewController
 
-static NSString * const reuseIdentifier = @"Cell";
+static NSString * const reuseIdentifier = @"MyCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,7 +23,7 @@ static NSString * const reuseIdentifier = @"Cell";
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+//    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
     
     // Do any additional setup after loading the view.
 }
@@ -41,14 +41,12 @@ static NSString * const reuseIdentifier = @"Cell";
 #pragma mark <UICollectionViewDataSource>
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of items
-    return 0;
+    return 1;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
